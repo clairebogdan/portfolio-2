e323c93ed5520d476b769c440b64ba09dbd821be
clairebogdan <bogdanclaire@gmail.com>
2022-09-10 21:48:34 -0400

    wip new website

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app/app.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    NavigationComponent,
+    ContactComponent,
+    PortfolioComponent,
+    AboutComponent,
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
